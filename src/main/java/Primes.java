f3e62cf258116693928bc213b4920e2faa235ce8
edsel090 <hernandezdanny936@gmail.com>
2021-01-11 06:12:10 +0000

    what did you change?

--- src/main/java/Primes.java
+++ src/main/java/Primes.java
@@ -1,19 +1,33 @@
 public class Primes {
     public static int countFactors(int n){
         int factors = 0;
-        // replace with your code
+        for(int i = 1; i <= 8; i++){
+          if(n%i==0 && i*i !=n){
+            factors += 2;
+          }else if(i*i==n){
+            factors++;
+          }
+        System.out.println(factors);
+        }
         return factors;
+      
     }
 
     public static boolean isPrime(int n){
+        int factors = countFactors(n);
         boolean prime = true;
-        // replace with your code
+        for(int i = 1; i <= factors; i++){
+          if(i%n==0){
+            prime = false;
+          }
+        }
+        System.out.println(prime);
         return prime;
     }
 
     public static int countPrimes(int n){
         int numPrimes = 0;
-        // replace with your code
+        
         return numPrimes;
     }
 }
